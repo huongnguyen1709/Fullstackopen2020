f0ed1ad2b1f3855a41e1eba9b7562ee6ebc8fd95
huongng1709 <huongnguyen.nth1709@gmail.com>
2020-10-26 15:45:05 +0200

    finish exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filterName, onHandleChange }) => {
     return (
@@ -47,15 +48,20 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-    var [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    var [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterName, setFilterName] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
 
     const onHandleSubmit = e => {
         e.preventDefault()
