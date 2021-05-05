e447729cb81207a193d38a483250d51ca52612d2
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-05-05 13:46:52 +0300

    submit exe 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,12 +4,20 @@ const app = require('../app');
 
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
+test('blog posts are returned as json', async () => {
   jest.setTimeout(10000);
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+test('the unique identifier property of the blog posts named id is defined', async () => {
+  jest.setTimeout(20000);
+  await api.get('/api/blogs'),
+    (request, response) => {
+      expect(response && response.every((blog) => blog && blog.id)).toBe(true);
+    };
 });
 
 afterAll(() => {
