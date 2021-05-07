28bcadf47beaa1b7be5df1abdfad07eaceb32d35
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-05-07 21:29:24 +0300

    submit exe 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
@@ -31,11 +23,9 @@ blogsRouter.get('/:id', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-
-  if (!token || !decodedToken.id) {
+  if (!decodedToken.id) {
     console.log('hello');
     return response.status(401).json({ error: 'token missing or invalid' });
   }
