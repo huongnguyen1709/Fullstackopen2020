2bf42e830f1f9380fca93339d505a214bbe1fa6b
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-05-20 11:30:44 +0300

    submit exe 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const detailStyle = {
+    display: showDetail ? '' : 'none',
+  };
+
+  const inlineBlock = {
+    display: 'inline-block',
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} &nbsp;
+      <button onClick={() => setShowDetail(!showDetail)}>
+        {showDetail ? 'hide' : 'view'}
+      </button>
+      <div style={detailStyle}>
+        <div>{blog.url}</div>
+        <div style={inlineBlock}>likes {blog.likes}</div> &nbsp;
+        <button>like</button>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
