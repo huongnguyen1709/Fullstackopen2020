337935e621dc8aee7dccd90aaa1cd6d341a03315
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-05-22 14:33:12 +0300

    submit exe 5.12

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,47 +1,47 @@
-import axios from 'axios';
-import jwtdecode from 'jwt-decode';
-const baseUrl = '/api/blogs';
+import axios from 'axios'
+import jwtdecode from 'jwt-decode'
+const baseUrl = '/api/blogs'
 
-let token = null;
+let token = null
 
 const setToken = (newToken) => {
-  token = `bearer ${newToken}`;
-};
+  token = `bearer ${newToken}`
+}
 
 const getAll = async () => {
-  const request = await axios.get(baseUrl);
-  return request.data;
-};
+  const request = await axios.get(baseUrl)
+  return request.data
+}
 
 const create = async (newBlog) => {
   const config = {
     headers: { Authorization: token },
-  };
+  }
 
-  const response = await axios.post(baseUrl, newBlog, config);
-  return response.data;
-};
+  const response = await axios.post(baseUrl, newBlog, config)
+  return response.data
+}
 
 const update = async (updatedBlog) => {
-  const response = await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog);
-  return response.data;
-};
+  const response = await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog)
+  return response.data
+}
 
 const deleteBlog = async (id) => {
   const config = {
     headers: { Authorization: token },
-  };
-  const response = await axios.delete(`${baseUrl}/${id}`, config);
-  console.log(response.data);
-  return response.data;
-};
+  }
+  const response = await axios.delete(`${baseUrl}/${id}`, config)
+  console.log(response.data)
+  return response.data
+}
 
 const getUserId = async () => {
-  const decodedToken = jwtdecode(token);
-  const userId = decodedToken.id;
+  const decodedToken = jwtdecode(token)
+  const userId = decodedToken.id
 
-  return userId;
-};
+  return userId
+}
 
 export default {
   getAll,
@@ -50,4 +50,4 @@ export default {
   update,
   deleteBlog,
   getUserId,
-};
+}
