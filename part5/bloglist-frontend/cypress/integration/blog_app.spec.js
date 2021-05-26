1d032e4ea52d5137b8c8e7ac91d75e2a039bc18e
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-05-26 19:19:19 +0300

    submit exe 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,42 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen',
+    };
+    cy.request('POST', 'http://localhost:3003/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
   it('Login form is shown', function () {
     cy.contains('log in').click();
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('log in').click();
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('salainen');
+      cy.get('#login-button').click();
+
+      cy.contains('Matti Luukkainen logged-in');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.contains('log in').click();
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+
+      cy.contains('Wrong username or password');
+      cy.get('.notification')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid');
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in');
+    });
   });
 });
