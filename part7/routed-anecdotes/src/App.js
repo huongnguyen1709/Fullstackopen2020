5113b7126b90ab5aa23553f2d9ac9b916dba294d
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-06-03 15:42:27 +0300

    submit exe 7.5 & exe 7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -107,24 +107,44 @@ const CreateNew = (props) => {
     history.push('/');
   };
 
+  const resetField = () => {
+    content.reset(), author.reset(), info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...content} />
+          <input
+            name='content'
+            value={content.value}
+            type={content.type}
+            onChange={content.onChange}
+          />
         </div>
         <div>
           author
-          <input name='author' {...author} />
+          <input
+            name='author'
+            value={author.value}
+            type={author.type}
+            onChange={author.onChange}
+          />
         </div>
         <div>
           url for more info
-          <input name='info' {...info} />
+          <input
+            name='info'
+            value={info.value}
+            type={info.type}
+            onChange={info.onChange}
+          />
         </div>
         <button>create</button>
       </form>
+      <button onClick={resetField}>reset</button>
     </div>
   );
 };
