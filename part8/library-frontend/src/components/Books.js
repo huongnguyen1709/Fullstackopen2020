7fd67d70ff3492ad03b4c9da38a51f3573fd8f30
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-06-29 17:06:38 +0300

    submit exe 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [filterBooks, setFilterBooks] = useState(null);
   if (!props.show) {
     return null;
   }
@@ -14,6 +15,51 @@ const Books = (props) => {
   }
 
   books = result.data.allBooks;
+
+  const showGenreBooks = (genre) => {
+    const booksFilter = books.filter((book) => {
+      const found = book.genres.find((g) => g === genre);
+      if (found) return book;
+      else return null;
+    });
+    setFilterBooks(booksFilter);
+  };
+
+  const onGenresList = () => {
+    const genresList = [];
+    books.map((book) => {
+      book.genres.map((genre) => {
+        const item = genresList.find((g) => g === genre);
+        if (!item) genresList.push(genre);
+        else return null;
+      });
+    });
+    return genresList.map((genre) => (
+      <button key={genre} onClick={() => showGenreBooks(genre)}>
+        {genre}
+      </button>
+    ));
+  };
+
+  const booksList = () => {
+    if (filterBooks) {
+      return filterBooks.map((a) => (
+        <tr key={a.title}>
+          <td>{a.title}</td>
+          <td>{a.author.name}</td>
+          <td>{a.published}</td>
+        </tr>
+      ));
+    } else {
+      return books.map((a) => (
+        <tr key={a.title}>
+          <td>{a.title}</td>
+          <td>{a.author.name}</td>
+          <td>{a.published}</td>
+        </tr>
+      ));
+    }
+  };
 
   return (
     <div>
@@ -26,15 +72,14 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {booksList()}
         </tbody>
       </table>
+
+      <div>
+        {onGenresList()}
+        <button onClick={() => setFilterBooks(null)}>all genres</button>
+      </div>
     </div>
   );
 };
