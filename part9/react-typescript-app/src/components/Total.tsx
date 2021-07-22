a918ac0f00198a6aca80331fca324457612bd3bd
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-07-22 14:03:13 +0300

    submit exe 9.14

---
+++ part9/react-typescript-app/src/components/Total.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface coursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const Total = ({ courseParts }: { courseParts: coursePart[] }) => {
+  return (
+    <p>
+      Number of exercises{' '}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  );
+};
+
+export default Total;
